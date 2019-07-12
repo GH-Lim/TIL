f8dd6724b9f22025797601173482109304519632
GH-Lim <gunhyuck11@gmail.com>
2019-07-12 10:00:34 +0900

    Finish Lotto

--- startCamp/04_day/flask/app.py
+++ startCamp/04_day/flask/app.py
@@ -73,30 +73,53 @@ def lotto_result():
     response = requests.get(url)
     lotto_info = response.json() # Json 타입의 파일을 python dictionary로 parsing 해줘!
 
-    drwtNo = [lotto_info["drwtNo1"], lotto_info["drwtNo2"], lotto_info["drwtNo3"],
-    lotto_info["drwtNo4"], lotto_info["drwtNo5"], lotto_info["drwtNo6"]]
+    drwtNo = []
+    for i in range(1, 7):
+        drwtNo.append(lotto_info[f'drwtNo{i}']) # f_string을 이용하여 for문으로
+    # drwtNo.append(str(lotto_info[f'drwtNo{i}'])) str로 비교하려 할 경우 이렇게 할 수 있다!
+
+    # drwtNo = [lotto_info["drwtNo1"], lotto_info["drwtNo2"], lotto_info["drwtNo3"],
+    # lotto_info["drwtNo4"], lotto_info["drwtNo5"], lotto_info["drwtNo6"]]
     bnusNo = lotto_info["bnusNo"]
 
-    drwt_set = set(drwtNo)
-    nums_set = set(lotto_intnum)
+    drwt_set = set(drwtNo)              # 당첨번호
+    nums_set = set(lotto_intnum)        # 사용자입력번호
+    samenum = list(drwt_set & nums_set) # 교집합
+    
 
-    if len(drwt_set & nums_set) == 6:
-        prize = '1등'
-    elif len(drwt_set & nums_set) == 5:
-        if bnusNo in lotto_intnum:
-            prize = '2등'
+    if len(lotto_intnum) == 6:          # 사용자가 보낸 번호가 올바른지 확인 (6개가 맞는지)
+
+        if len(samenum) == 6:
+            prize = '1등'
+        elif len(samenum) == 5:
+            if bnusNo in lotto_intnum:
+                prize = '2등'
+                samenum.append(bnusNo)
+            else:
+                prize = '3등'
+        elif len(samenum) == 4:
+            prize = '4등'
+        elif len(samenum) == 3:
+            prize = '5등'
         else:
-            prize = '3등'
-    elif len(drwt_set & nums_set) == 4:
-        prize = '4등'
-    elif len(drwt_set & nums_set) == 3:
-        prize = '5등'
+            prize = '낙첨'
+
+        # matched = 0
+        # for number in lotto_numbers:
+        #   if number in winner:
+        #       matched += 1            # 당첨시 matched 를 1씩 증가 시킨다.
+        # 
     else:
-        prize = '낙첨'
+        prize = '올바르지 않은 번호'
+        samenum = []
 
+    samenum.sort()
+    drwtNo.sort()
+    lotto_intnum.sort()
 
-
-    return f'{lotto_info["drwNo"]} 회 동행로또\n{prize} 입니다.'
+    return render_template('lotto_result.html', lotto_round=lotto_round, prize=prize, samenum=samenum,
+    bnusNo=bnusNo, drwtNo=drwtNo, lotto_intnum=lotto_intnum)
+#    return f'{lotto_info["drwNo"]} 회 동행로또\n{prize} 입니다.'
 
 
 if __name__ == '__main__':# 메인으로 활용할 때만 디버그 모드를 on 하겠다
